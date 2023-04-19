e5cd7f3fb74c01bc3705e07a1ceea658491e5a19
Mr.Hope <mister-hope@outlook.com>
2023-04-19 14:48:31 +0800

    chore: update deps

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,9 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
+  const { name = "World" } = req.query;
+
   return res.json({
     message: `Hello ${name}!`,
-  })
+  });
 }
